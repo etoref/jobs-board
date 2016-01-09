bdeb01cc136a1a90f150ae9747e4f699c9d8f16f
Etore Sales Fodra <etoref@gmail.com>
2016-01-08 22:51:05 -0200

    adding bootstrap for page responsive layout

--- Models.js
+++ Models.js
@@ -4,14 +4,6 @@ var JobCollection = Backbone.Collection.extend({
   model: JobModel,
   url: 'https://api.greenhouse.io/v1/boards/credible/embed/jobs',
   sync : function(method, collection, options) {
-    // By setting the dataType to "jsonp", jQuery creates a function
-    // and adds it as a callback parameter to the request, e.g.:
-    // [url]&callback=jQuery19104472605645155031_1373700330157&q=bananarama
-    // If you want another name for the callback, also specify the
-    // jsonpCallback option.
-    // After this function is called (by the JSONP response), the script tag
-    // is removed and the parse method is called, just as it would be
-    // when AJAX was used.
     options.dataType = "jsonp";
     return Backbone.sync(method, collection, options);
   },
@@ -32,4 +24,18 @@ var JobCollection = Backbone.Collection.extend({
       alert("Hey, removing an element! " + model.get('title'));
     });
   }
+});
+
+var JobDescriptionModel = Backbone.Model.extend({
+
+    initialize: function(options){
+      this.options = options;
+      console.log(options.url);
+      this.url = "https://api.greenhouse.io/v1/boards/credible/embed/job?id="+options.url.match(/(\d+)$/g);
+      console.log(options);
+    },
+
+    url: function(){
+      return"https://api.greenhouse.io/v1/boards/credible/embed/job?id=104674"
+    },
 });
--- Views.js
+++ Views.js
@@ -1,15 +1,11 @@
 var JobListView = Backbone.View.extend({
 
-    el: '#job_list_template',
-
-    initialize: function(){
+    initialize: function(config){
+        this.config = config;
+        this.collection = config.collection;
         this.listenTo(this.collection,'add', this.onModelAdd);
     },
 
-    render: function(){
-        alert("Rendering collection view");
-    },
-    
     onModelAdd: function(model){
         itemView = new JobItemView({model: model});
         this.$el.append(itemView.render());
@@ -20,7 +16,16 @@ var JobListView = Backbone.View.extend({
 var JobItemView = Backbone.View.extend({
 
     render: function(){
-        var compiled = _.template('<li><a href=<%= _.escape(absolute_url) %> target="_blank"><%= _.escape(title) %></a></li>');
+        var compiled = _.template($("#job_list_item_template").html());
         return this.$el.html(compiled(this.model.toJSON()));
-    }
+    },
+});
+
+var JobDescriptionView = Backbone.View.extend({
+
+    render: function(){
+        var content = this.model.toJSON().content;
+        var compiled = _.template(content);
+        return this.$el.html(compiled());
+    },
 });
